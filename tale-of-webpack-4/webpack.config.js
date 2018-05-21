efcfc93f45cd3d65142c96cfa4d3dd208931a850
Andrei Fidelman <andrei.fidelman@actum.cz>
2018-05-21 21:58:36 +0200

    tale-of-webpack-4: Solve hashing problem

--- tale-of-webpack-4/webpack.config.js
+++ tale-of-webpack-4/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -19,18 +20,23 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+        use:  [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
   plugins: [ 
-    new ExtractTextPlugin({
-      filename: 'style.[chunkhash].css',
-      disable: false,
-      allChunks: true
+    // new ExtractTextPlugin({
+    //   filename: 'style.[hash].css',
+    //   disable: false,
+    //   allChunks: true
+    // }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
